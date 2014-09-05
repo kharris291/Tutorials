d346b7d1d0b8896eef21afd59f10c943d6342cc7
kharris291 <harriskevin123@gmail.com>
2014-09-05 12:55:11 +0100

    tut 5

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ var myApp = angular.module('myApp',[])
         return {message:"I'm data from a service"}
     })
 
+myApp.filter('reverse', function(Data){
+    return function(text){
+        return text.split("").reverse().join("") + Data.message;
+    }
+})
+
 function FirstCtrl($scope, Data){
     $scope.data = Data;
 }
