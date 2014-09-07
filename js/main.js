42deb7bc6950c79454273f3898382e7130af6348
kharris291 <harriskevin123@gmail.com>
2014-09-07 06:50:44 +0100

    directives

--- js/main.js
+++ js/main.js
@@ -7,73 +7,31 @@ var myApp = myApp || {};
 
 myApp = angular.module('myApp',[])
 
-myApp.factory('Avengers', function() {
-    var Avengers = {};
-    Avengers.cast = [
-        {
-            name: "Robert Downey Jr.",
-            character: "Tony Stark / Iron Man"
-        },
-        {
-            name: "Chris Evans",
-            character: "Steve Rogers / Captain America"
-        },
-        {
-            name: "Mark Ruffalo",
-            character: "Bruce Banner / The Hulk"
-        },
-        {
-            name: "Chris Hemsworth",
-            character: "Thor"
-        },
-        {
-            name: "Scarlett Johansson",
-            character: "Natasha Romanoff / Black Widow"
-        },
-        {
-            name: "Jeremy Renner",
-            character: "Clint Barton / Hawkeye"
-        },
-        {
-            name: "Tom Hiddleston",
-            character: "Loki"
-        },
-        {
-            name: "Clark Gregg",
-            character: "Agent Phil Coulson"
-        },
-        {
-            name: "Cobie Smulders",
-            character: "Agent Maria Hill"
-        },
-        {
-            name: "Stellan Skarsgard",
-            character: "Selvig"
-        },
-        {
-            name: "Samuel L. Jackson",
-            character: "Nick Fury"
-        },
-        {
-            name: "Gwyneth Paltrow",
-            character: "Pepper Potts"
-        },
-        {
-            name: "Paul Bettany",
-            character: "Jarvis (voice)"
-        },
-        {
-            name: "Alexis Denisof",
-            character: "The Other"
-        },
-        {
-            name: "Tina Benko",
-            character: "NASA Scientist"
-        }
-    ];
-    return Avengers;
+myApp.directive("superman", function(){
+    return{
+        restrict:"E",
+        template:"<div>Here I am to save the day</div>"
+    }
+
 })
 
-function AvengersCtrl($scope, Avengers) {
-    $scope.avengers = Avengers;
-}
+myApp.directive("superman1", function(){
+    return{
+        restrict:"A",
+        link: function(){
+            alert("I am working stronger!")
+        }
+    }
+
+})
+
+myApp.directive("flash", function(){
+    return{
+        restrict:"A",
+        link: function(){
+            alert("I am working faster!")
+        }
+    }
+
+})
+
